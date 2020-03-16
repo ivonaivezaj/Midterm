7494d4b1d69daa67a890702df6e919149b17a6a9
Ankur <vasavada.ankur@gmail.com>
2020-03-16 19:46:46 -0400

    added dummy transactions

---
+++ budget-buddy.js
@@ -0,0 +1,8 @@
+// const dummyTransactions = [
+// 	{ id: 1, desc: 'Flower', category: 'food', amount: -20 },
+// 	{ id: 2, desc: 'Salary', category: 'income', amount: 300 },
+// 	{ id: 3, desc: 'Book', category: 'clothing', amount: -10 },
+// 	{ id: 4, desc: 'Camera', category: 'bills', amount: -150 },
+// 	{ id: 5, desc: 'Movie', category: 'entertainment', amount: -50 },
+// 	{ id: 6, desc: 'Freelance', category: 'income', amount: 150 },
+// ];
